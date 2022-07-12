81fa3ab8c7091bbb5faf9af1005982098f786fc8
0122514 <liuhuimin@bonc.com.cn>
2022-07-12 21:39:04 +0800

    demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,54 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div @click="changeBtn">{{ pageChange }}</div>
+    <float-btn />
   </div>
 </template>
 
 <script>
+import FloatBtn from './floatBtn.vue'
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    msg: String,
+  },
+  components: {
+    FloatBtn,
+  },
+  data() {
+    return {
+      pageChange: '放大页面',
+      scal: '1',
+    }
+  },
+  methods: {
+    changeBtn() {
+      let getBody = document.body.style
+      if (this.pageChange == '放大页面') {
+        // Firefox
+        if (navigator.userAgent.indexOf('Chrome') > 0) {
+          this.scal = (parseFloat(this.scal) + 0.5).toFixed(2)
+          getBody.transform = `scale(${this.scal})`
+          getBody.transformOrigin = 'center top'
+        } else {
+          getBody.setProperty('zoom', '1.5')
+        }
+        this.pageChange = '正常页面'
+      } else {
+        // Firefox
+        if (navigator.userAgent.indexOf('Chrome') > 0) {
+          this.scal = (parseFloat(this.scal) - 0.5).toFixed(2)
+          getBody.transform = `scale(${this.scal})`
+          getBody.transformOrigin = '0.0'
+        } else {
+          getBody.removeProperty('zoom')
+        }
+        this.pageChange = '放大页面'
+      }
+    },
+  },
 }
 </script>
 
---
+++ src/components/floatBtn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="box">
+    <h2 class="text">固定的子元素</h2>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {},
+  computed: {},
+  methods: {},
+}
+</script>
+<style scoped>
+.box {
+  position: fixed;
+  top: 60px;
+  right: 30px;
+  text-align: center;
+  width: 100px;
+  height: 100px;
+  z-index: 999;
+  background: rgb(173, 213, 253);
+}
+.text {
+}
+</style>
